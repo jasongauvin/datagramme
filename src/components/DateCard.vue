244010341b336c70431bcb5eb4ab0ee5565c94b7
jasongauvin <contact.jason.gauvin@gmail.com>
2019-08-24 15:34:11 +0200

    add datecard component and page 1 but i broke my search function...

---
+++ src/components/DateCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="history">
+    <h2>A MOMENT OF HISTORY</h2>
+    <p>You entered {{dates.length}} dates...</p>
+  </div>
+    <li class="date">
+      <h2>{{date.date}}</h2>
+      <p>{{date.text}}</p>
+    </li>
+</template>
+
+<script>
+export default {
+  name: "DateCard",
+</script>
+
+<style lang="scss" scoped>
+.history{
+  margin-top: 50px;
+  margin-bottom: 20px;
+  background: black;
+  padding: 20px;
+  text-align: center;
+  border-radius: 10px;
+  color: white;
+  h2{
+    margin-bottom: 5px;
+  }
+  p{
+    font-weight: bold;
+  }
+}
+
+.date{
+  padding: 20px;
+  background: white;
+  border-radius: 10px;
+  color: black;
+
+  &:not(:last-child){
+    margin-bottom: 20px;
+  }
+
+  h2{
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,142 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="form">
+      <form>
+        <label>
+          Please enter dates separates by commas (ex : 4/3, 12/2, 3/5, ...):
+          <input
+            v-model="tag"
+            type="text"
+            @keyup.enter="search($event)"
+          />
+        </label>
+      </form>
+    </div>
+    <div class="wrapper">
+      <p v-if="loading">Loading...</p>
+      <ul v-else class="image-card">
+        <!-- <DateCard class="date" v-for="(date, index) in dates" :key="index"/> -->
+        <li v-for="date in dates" :key="date.index">{{date}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+// const CancelToken = axios.CancelToken;
+// const source = CancelToken.source();
+// import DateCard from "@/components/DateCard.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    // DateCard
+  },
+  data() {
+    return {
+      loading: false,
+      tag: "",
+      dates: []
+    };
+  },
+  methods: {
+    search(event) {
+      console.log("Searching for: ", this.tag);
+      this.dates = [];
+      let values = event.target.value;
+      console.log(values);
+      // let valuesArray = values.split(", ");
+      this.loading = true;
+      // valuesArray.forEach(element => {
+      //   axios
+      //     .get(`http://numbersapi.com/${element}/date`, {
+      //       cancelToken: source.token
+      //     })
+      //     .catch(function(thrown) {
+      //       if (axios.isCancel(thrown)) {
+      //         console.log("Request canceled", thrown.message);
+      //       }
+      //     })
+      //     .then(response =>
+      //       this.dates.push({
+      //         date: element,
+      //         text: response.data
+      //       })
+      //     )
+      //     .catch(error => {
+      //       console.log("An error ocurred: ", error);
+      //     });
+      // });
+    },
+    
+    // fetchDate(element) {
+    //   axios
+    //     .get(`http://numbersapi.com/${element}/date`, {
+    //       cancelToken: source.token
+    //     })
+    //     .catch(function(thrown) {
+    //       if (axios.isCancel(thrown)) {
+    //         console.log("Request canceled", thrown.message);
+    //       }
+    //     })
+    //     .then(response =>
+    //       this.dates.push({
+    //         date: element,
+    //         text: response.data
+    //       })
+    //     )
+    //     .catch(error => {
+    //       console.log("An error ocurred: ", error);
+    //     });
+    // }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.form {
+  padding-top: 50px;
+  input {
+    width: 100%;
+    border: none;
+    background: none;
+    border-bottom: 2px solid;
+    margin-top: 20px;
+    text-align: center;
+    color: black;
   }
 }
-</script>
+
+.text-centered {
+  text-align: center;
+}
+.wrapper {
+  margin: 0 auto;
+  max-width: 800px;
+  @media only screen and (max-width: 799px) {
+    max-width: 100%;
+    margin: 0 1.5rem;
+  }
+}
+.image-card {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+
+.dates {
+  margin-top: 20px;
+}
+
+.btn-go {
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  border-radius: 20px;
+  background: transparent;
+  border: none;
+}
+</style>
